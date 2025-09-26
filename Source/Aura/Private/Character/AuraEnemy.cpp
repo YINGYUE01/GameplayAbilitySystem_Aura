a49491fdba7b9707304b6073ea31dd7f07cfb581
YINGYUE <2208557365@qq.com>
2025-09-26 17:10:44 +0800

    Post Process Highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,26 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+	
+}
+
 void AAuraEnemy::HighlightActor()
 {
 	bHighlight = true;
+	GetMesh()->SetRenderCustomDepth(bHighlight);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	Weapon->SetRenderCustomDepth(bHighlight);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
 	bHighlight = false;
+	GetMesh()->SetRenderCustomDepth(bHighlight);
+	Weapon->SetRenderCustomDepth(bHighlight);
 }
