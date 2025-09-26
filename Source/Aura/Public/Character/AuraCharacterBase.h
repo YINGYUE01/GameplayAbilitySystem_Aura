077c72ef0784572e6dbc5c286ce7bbcb52df1bd6
YINGYUE <2208557365@qq.com>
2025-09-26 14:15:01 +0800

    Character Blueptint Setup

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
-
+class USkeletalMeshComponent;
 UCLASS(Abstract)
 class AURA_API AAuraCharacterBase : public ACharacter
 {
@@ -16,6 +16,8 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-	
+
+	UPROPERTY(EditAnywhere,Category="Weapon")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 
 };
