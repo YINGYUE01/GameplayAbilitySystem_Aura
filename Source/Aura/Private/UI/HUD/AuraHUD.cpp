7a1862423e09d48cba51d807030fc7b05ff45b07
YINGYUE <2208557365@qq.com>
2025-09-30 14:21:37 +0800

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+// Copyright YING
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(),OverlayWidgetClass);
+	Widget->AddToViewport();
+}
