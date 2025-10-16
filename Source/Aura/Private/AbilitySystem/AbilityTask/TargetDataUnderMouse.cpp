a3382e8bc493edbb6412a796fb1eb605c2f2c31f
YINGYUE <2208557365@qq.com>
2025-10-16 15:51:47 +0800

    Send  Mouse Cursor Data

--- Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/AbilityTask/TargetDataUnderMouse.h"
 
+#include "AbilitySystemComponent.h"
+
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
 	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
@@ -12,10 +14,41 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
-	FHitResult HitResult;
+	if (Ability->GetCurrentActorInfo()->IsLocallyControlled())
+	{
+		SendMouseCursorData();
+	}
+	else
+	{
+		
+	}
+
+	
+
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	FScopedPredictionWindow PredictionWindow(AbilitySystemComponent.Get());
+	
+	FGameplayAbilityTargetDataHandle DataHandle;
+	FHitResult CursorHit;
 	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
-	PC->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility,false,HitResult);
-	//Cast<APlayerController>(GetOwnerActor())
-	FVector MouseLocation = HitResult.Location;
-	ValidData.Broadcast(MouseLocation);
+	PC->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility,false,CursorHit);
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult = CursorHit;
+	DataHandle.Add(Data);
+
+	AbilitySystemComponent->ServerSetReplicatedTargetData(
+		GetAbilitySpecHandle(),
+		GetActivationPredictionKey(),
+		DataHandle,
+		FGameplayTag(),
+		AbilitySystemComponent->ScopedPredictionKey
+		);
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
+	
 }
