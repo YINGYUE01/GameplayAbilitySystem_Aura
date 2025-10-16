75500e14306ac78a3aad33f953e41f42fa514783
YINGYUE <2208557365@qq.com>
2025-10-16 16:55:36 +0800

    Receving Target Data

--- Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
@@ -14,27 +14,33 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
-	if (Ability->GetCurrentActorInfo()->IsLocallyControlled())
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+	if (bIsLocallyControlled)
 	{
 		SendMouseCursorData();
 	}
 	else
 	{
-		
+		const FGameplayAbilitySpecHandle AbilitySpecHandle = GetAbilitySpecHandle();
+		const FPredictionKey ActivationPredicationKey = GetActivationPredictionKey();
+		AbilitySystemComponent.Get()->AbilityTargetDataSetDelegate(AbilitySpecHandle,ActivationPredicationKey).AddUObject(this,&UTargetDataUnderMouse::OnTargetDataReplicatedCallback);
+		const bool bCalledDelegate = AbilitySystemComponent.Get()->CallReplicatedTargetDataDelegatesIfSet(AbilitySpecHandle,ActivationPredicationKey);
+		if (!bCalledDelegate)
+		{
+			SetWaitingOnRemotePlayerData();
+			
+		}
 	}
-
-	
-
 }
 
 void UTargetDataUnderMouse::SendMouseCursorData()
 {
-	FScopedPredictionWindow PredictionWindow(AbilitySystemComponent.Get());
-	
-	FGameplayAbilityTargetDataHandle DataHandle;
-	FHitResult CursorHit;
+	FScopedPredictionWindow PredictionWindow(AbilitySystemComponent.Get(),GetActivationPredictionKey(),true);
 	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult CursorHit;
+	
 	PC->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility,false,CursorHit);
+	FGameplayAbilityTargetDataHandle DataHandle;
 	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
 	Data->HitResult = CursorHit;
 	DataHandle.Add(Data);
@@ -51,4 +57,14 @@ void UTargetDataUnderMouse::SendMouseCursorData()
 		ValidData.Broadcast(DataHandle);
 	}
 	
+}
+
+void UTargetDataUnderMouse::OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle,
+	FGameplayTag ActivationTag)
+{
+	AbilitySystemComponent->ConsumeClientReplicatedTargetData(GetAbilitySpecHandle(),GetActivationPredictionKey());
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
 }
--- Source/Aura/Private/AuraAssetManager.cpp
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -3,6 +3,7 @@
 
 #include "AuraAssetManager.h"
 #include "AuraGameplayTags.h"
+#include "AbilitySystemGlobals.h"
 UAuraAssetManager& UAuraAssetManager::Get()
 {
 	check(GEngine);
@@ -14,4 +15,5 @@ void UAuraAssetManager::StartInitialLoading()
 {
 	Super::StartInitialLoading();
 	FAuraGameplayTags::InitializeNativeTags();
+	UAbilitySystemGlobals::Get().InitGlobalData();
 }
