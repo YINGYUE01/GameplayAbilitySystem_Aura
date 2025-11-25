a3f6ded712c98930aaa1637fb92bff891f92ad82
YINGYUE <2208557365@qq.com>
2025-11-25 17:32:36 +0800

    Blackboard Keys

--- Source/Aura/Private/AI/BTService_FindNearestPLayer.cpp
+++ Source/Aura/Private/AI/BTService_FindNearestPLayer.cpp
@@ -4,10 +4,19 @@
 #include "AI/BTService_FindNearestPLayer.h"
 
 #include "AIController.h"
+#include "Kismet/GameplayStatics.h"
 
 void UBTService_FindNearestPLayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 	GEngine->AddOnScreenDebugMessage(1,1.f,FColor::Red,*AIOwner->GetName());
 	GEngine->AddOnScreenDebugMessage(2,1.f,FColor::Green,*ActorOwner->GetName());
+	APawn* OwningPawn = AIOwner->GetPawn();
+	const FName TargetTags = OwningPawn->ActorHasTag("Player") ? FName("Enemy") : FName("Player");
+	TArray<AActor*> ActorsWithTags;
+	UGameplayStatics::GetAllActorsWithTag(OwningPawn,TargetTags,ActorsWithTags);
+	for (auto& Actor : ActorsWithTags)
+	{
+		
+	}
 }
--- Source/Aura/Public/AI/BTService_FindNearestPLayer.h
+++ Source/Aura/Public/AI/BTService_FindNearestPLayer.h
@@ -15,4 +15,9 @@ class AURA_API UBTService_FindNearestPLayer : public UBTService_BlueprintBase
 	GENERATED_BODY()
 protected:
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector TargetToFollowSelector;
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector DistanceToTarget;
 };
